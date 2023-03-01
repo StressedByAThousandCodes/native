bd8a25b1c9129f7779e6adc12fbc5d1042988244
Marvin <marvinvillalon23@gmail.com>
2023-03-01 11:04:04 +0800

    Files added

---
+++ App.js
@@ -0,0 +1,67 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+
+  const [email, setEmail] =useState('');
+  const [password, setPassword] = useState('');
+  const [displayText, setDisplayText] = useState('');
+  
+  const printInputs = () => {
+    setDisplayText(`Email: ${email} \nPassword: ${password}`)
+  }
+  const clear = () => {
+    {setDisplayText('')}
+    {setEmail('')}
+    {setPassword('')}
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Log In</Text>
+      <View>
+        <TextInput 
+          style={styles.text} 
+          placeholder='Enter Email' 
+          onChangeText={text => setEmail(text)}
+          value={email}
+        />
+        <TextInput 
+          style={styles.text} 
+          placeholder='Enter Password'
+          onChangeText= {text => setPassword(text)}
+          value={password}
+          secureTextEntry = {true}
+        />
+        <Button 
+          title='Submit'
+          onPress={printInputs}
+        />
+        <Text>{displayText}</Text>
+        <Button 
+          title='Clear'
+          onPress={clear}
+        />
+
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    borderColor: 'black',
+    borderWidth: 2,
+    width: 200,
+    padding: 10,
+    margin: 10,
+  }
+});
